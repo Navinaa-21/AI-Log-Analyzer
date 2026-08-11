46479074b594df9c1011eb8e29b028d1e51dabae
navinaa-03 <navinaakalaichelvan03@gmail.com>
2026-08-11 11:25:51 +0530

    refactor: replace triangle area calculation script with factorial calculation logic

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
-# Python Program to find the area of triangle
-
-a = 5
-b = 6
-c = 7
-
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
-
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+n = 6
+if n < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    print(f)
