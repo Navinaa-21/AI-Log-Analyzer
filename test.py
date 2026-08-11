4ecdaa023a13366ecfa58cca7b9755d79f15c2dc
navinaa-03 <navinaakalaichelvan03@gmail.com>
2026-08-11 15:35:21 +0530

    refactor: replace factorial calculation logic with a simple print statement in test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-n = 6
-if n < 0:
-    print("Factorial is not defined for negative numbers")
-else:
-    f = 1
-    for i in range(1, n+1):
-        f *= i
-    print(f)
+print("just printing for testing purpose")
